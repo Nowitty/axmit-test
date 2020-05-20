3f0f0e47a769e68ce6bfa2273d52cf9d8ac53063
Руслан Куга <ruslankuga@MacBook-Air.local>
2020-05-20 17:40:18 +0300

    fix

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php 
 
 $db = [
-    'host' =>
-    'name' =>
-    'user' =>
-    'pass' =>
-    'port' =>
+    'host' => 'ec2-35-171-31-33.compute-1.amazonaws.com',
+    'name' => 'd91vcn4speqqg2',
+    'user' => 'zdbtebkvfyplfe',
+    'pass' => '043de618653034288655120d843beae5d169edb4c41aae052d177e5ad4783cda',
+    'port' => '5432'
 ];
